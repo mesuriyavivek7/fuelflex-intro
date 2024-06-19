b4c2ba9712250e735c0aceccbff93241e9f360d2
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2024-06-19 18:36:07 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+//importig images
+import Logo from '../src/assets/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='content'>
+         <img className='logoimg' src={Logo} alt=''></img>
+         <p>fuelflex gives you premium quality peanut butter</p>
+         <h4>coming soon</h4>
+       </div>
     </div>
   );
 }
